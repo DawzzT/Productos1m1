32eca26a83256176fa134550560fa27dc549a47f
Walterddt1M1 <walddt05@gmail.com>
2021-09-08 08:45:26 -0600

    Iniciando Poryecto

---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity {get; set;}
+        public decimal Price { get; set; }
+        public  DateTime CaducityDate { get; set; }
+        public MeasurementUnit Unit { get; set; }
+
+    }
+}
---
+++ Infraestructure/Products/ProductModel.cs
@@ -0,0 +1,123 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infraestructure.Products
+{
+    class ProductModel
+    {
+        private Product[] productos;
+
+        #region CRUD
+
+
+        public void Add(Product p)
+        {
+            Add(p, ref productos);
+        }
+
+        public bool  Delete (Product p)
+        {
+            if(p == null)
+            {
+                throw new ArgumentException("El objeto no puede ser null");
+            }
+            int index = GetIndexById(p.Id);
+            if (index < 0)
+            {
+                throw new ArgumentException($"El producto con id {p.Id} no ha sido encontrado.");
+            }
+            if (index !=  productos.Length - 1)
+            {
+                productos[index] = productos[productos.Length - 1];
+            }
+            Product[] tmp = new Product[productos.Length - 1];
+            Array.Copy(productos, tmp, tmp.Length);
+            productos = tmp;
+            return productos.Length == tmp.Length;
+        }
+        public int Update(Product p)
+        {
+            if ( p == null)
+            {
+                throw new ArgumentException("El objeto no puede ser null");
+            }
+
+            int index = GetIndexById(p.Id);
+            if ( index < 0)
+            {
+                throw new ArgumentException($"El producto con id {p.Id} no ha sido encontrado.");
+            }
+            productos[index] = p;
+            return index;
+        }
+        public Product[] GetAll()
+        {
+            return productos;
+        }
+
+
+        #endregion
+
+        #region PrivateMethods
+
+
+        private int GetIndexById(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException("El Id no puede ser menor que 0");
+            }
+           int index = int.MinValue, i = 0;
+            if (productos == null)
+            {
+                return index;
+            }
+            foreach (Product p in productos)
+            {
+                if (p.Id == id)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            return index;
+        }
+
+        private void Add(Product p, ref Product[] pds)
+        {
+            if ( pds == null)
+            {
+                pds = new Product[1];
+                pds[pds.Length - 1] = p;
+                return;
+            }
+
+            Product[] tmp = new Product[pds.Length - 1];
+            Array.Copy(pds, tmp, pds.Length);
+            tmp[tmp.Length - 1] = p;
+            pds = tmp;
+        }
+
+
+        #endregion
+
+        #region Queries
+
+
+        public Product GetProductById(int id)
+        {
+            if (id < 0)
+            {
+                throw new ArgumentException($"El id {id} no es valido");
+            }
+            int index = GetIndexById(id);
+           
+            return index <= 0 ? null : productos[index];
+        }
+
+
+        #endregion
+    }
+}
